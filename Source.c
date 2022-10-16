932a7dbcaa6243f8059bd374057b9ba7b972dba8
anoddyne <anoddyne@outlook.com>
2022-10-16 12:03:58 +0300

    fixed the code

--- Source.c
+++ Source.c
@@ -5,28 +5,45 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#define n 3
 
 int main() {
 	srand(time(NULL));
-	int S[3][3], P[3][3];
-		double Q[3][3];
-	for (int i = 0; i < 3; i++) {
-		for (int j = 0; j < 3; j++) {
-			S[i][j] = 0 + rand() % 9;
-			P[i][j] = 0 + rand() % 9;
-			/*printf("%d ", S[i][j]);
-			printf("%d ", P[i][j]);*/
-			for (int k = 0; k < 3; k++) {
-				Q[i][j] += S[i][k] * P[k][j];
-				
+	int F[n][n], S[n][n], R[n][n];
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			F[i][j] = rand() % 10;
+			S[i][j] = rand() % 10;
+		}
+	}
 
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			R[i][j] = 0;
+			for (int k = 0; k < n; k++) {
+				R[i][j] += F[i][k] * S[k][j];
 			}
-			for (i = 0; i < 3; i++)
-			{
-				for (j = 0; j < 3; j++)
-					printf("%3d ", Q[i][j]);
-				printf("\n");
-			}
-		}//printf("\n");
+		}
+	}
+	printf("Output of the first matrix: \n");
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			printf("%d ", F[i][j]);
+		}
+		printf("\n");
+	}
+	printf("Output of the second matrix: \n");
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			printf("%d ", S[i][j]);
+		}
+		printf("\n");
+	}
+	printf("Result of matrices multiplication: \n");
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			printf("%d ", R[i][j]);
+		}
+		printf("\n");
 	}
 }
